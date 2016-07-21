0116c848bb40037a317e05e03f20fb8af06fc58a
windywany <windywany@163.com>
2016-07-21 17:14:56 +0800

    fix wula.php issue

--- wula.php
+++ wula.php
@@ -1,5 +1,12 @@
 <?php
-chmod ( 'tmp', '0777' );
+@ob_end_clean ();
+echo "php wula.php\n";
+echo "chmod ( 'tmp', 0777 )\n";
+flush ();
+chmod ( 'tmp', 0777 );
+$appid = rand ( 1, 10000000 );
+echo "generated appid: ", $appid, "\n";
+flush ();
 $content = file_get_contents ( 'bootstrap.php' );
-$content = str_replace ( "define ( 'APPID', 1 )", "define ( 'APPID', " . rand ( 1, 10000000 ) . " )", $content );
+$content = str_replace ( "define ( 'APPID', 1 )", "define ( 'APPID', " . $appid . " )", $content );
 file_put_contents ( 'bootstrap.php', $content );

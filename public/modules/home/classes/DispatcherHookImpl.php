885d8af0b7270b2b25ebd0621d0f36745d83a974
windywany <windywany@163.com>
2016-07-21 18:30:39 +0800

    change the priority of DemoDispatcher

--- public/modules/home/classes/DispatcherHookImpl.php
+++ public/modules/home/classes/DispatcherHookImpl.php
@@ -1,17 +1,18 @@
 <?php
+
 namespace home\classes;
 
 use wulaphp\router\hook\IRouterHooks;
 use wulaphp\plugin\Hook;
 
 class DispatcherHookImpl extends Hook implements IRouterHooks {
-
-    /**
-     * (non-PHPdoc)
-     *
-     * @see \wulaphp\router\hook\IRouterHooks::register()
-     */
-    public function register($router) {
-        $router->register ( new DemoDispatcher (), 1 );
-    }
+	
+	/**
+	 * (non-PHPdoc)
+	 *
+	 * @see \wulaphp\router\hook\IRouterHooks::register()
+	 */
+	public function register($router) {
+		$router->register ( new DemoDispatcher (), 100 );
+	}
 }
--- wula.php
+++ wula.php
@@ -1,10 +1,10 @@
 <?php
 @ob_end_clean ();
-echo "\t\tchmod ( 'tmp', 0777 )\n";
+echo "\tchmod ( 'tmp', 0777 )\n";
 flush ();
 chmod ( 'tmp', 0777 );
 $appid = rand ( 1, 10000000 );
-echo "\t\tgenerated appid: ", $appid, "\n";
+echo "\tgenerated appid: ", $appid, "\n";
 flush ();
 $content = file_get_contents ( 'bootstrap.php' );
 $content = str_replace ( "define ( 'APPID', 1 )", "define ( 'APPID', " . $appid . " )", $content );

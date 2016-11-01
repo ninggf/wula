9ca5e50d5a33a7533cb1508e83ba824b08391621
ningGf <windywany@qq.com>
2016-11-01 14:31:15 +0800

    auto-copy .env.example to .env

--- wula.php
+++ wula.php
@@ -6,6 +6,11 @@ chmod('tmp', 0777);
 echo "\tchmod ( 'logs', 0777 )\n";
 flush();
 chmod('logs', 0777);
+echo "\tcopy .env.example to .env\n";
+flush();
+$content = file_get_contents('.env.example');
+file_put_contents('.env', $content);
+
 $appid = rand(1, 10000);
 echo "\tgenerated appid: ", $appid, "\n";
 flush();

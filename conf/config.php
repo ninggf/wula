7e2e20a395a336972ca1cd4cadf42d4edd3ba9d0
windywany <windywany@163.com>
2016-12-08 15:34:12 +0800

    add resource.minify and resource.combinate config

--- conf/config.php
+++ conf/config.php
@@ -2,4 +2,11 @@
 /**
  * 应用配置文件.
  */
-return ['debug' => env('debug', DEBUG_DEBUG)];
+return [
+	'debug' => env('debug', DEBUG_DEBUG),
+
+    'resource' =>[
+    	'combinate'=>env('resource.combinate',0),
+        'minify'=>env('resource.minify',0)
+    ]
+];

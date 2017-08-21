f036bb8c0e9d081fb9a709d9946ac31ce198fa50
windywany <windywany@163.com>
2017-08-21 10:32:00 +0800

    update wulaphp to 1.4.0

--- crontab/cron.php
+++ crontab/cron.php
@@ -1,20 +1,8 @@
+#!/usr/bin/env php
 <?php
 /**
- * CRONTAB SCRIPT.
- */
+* CRONTAB SCRIPT.
+*/
 require __DIR__ . '/../bootstrap.php';
-
-if (isset($argv[1])) {
-	$clz = $argv[1];
-	if (is_subclass_of($clz, '\wulaphp\util\ICrontabJob')) {
-		/* @var wulaphp\util\ICrontabJob $cls */
-		$cls = new $clz();
-		exit(intval($cls->run()));
-	} else {
-		echo "'{$clz}' is not a valid crontab job.\n";
-		exit(1);
-	}
-} else {
-	fire('crontab', time());
-}
+fire('crontab', time());
 //that's all.

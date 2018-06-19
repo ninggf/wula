9819c07232601aa416d472f15e9e8b330a9d5a57
windywany <windywany@163.com>
2018-06-19 18:52:51 +0800

    alias added

--- modules/alias.php
+++ modules/alias.php
@@ -7,7 +7,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-
-return [
-	'add' => 'home/add'
-];
+// your alias here
+//return [
+//	'add' => 'home/add'
+//];

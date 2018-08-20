d047138692d3662934ff8e4c8697aed656aed6dd
windywany <windywany@163.com>
2018-08-20 13:50:45 +0800

    ready for 2.0

--- bootstrap.php
+++ bootstrap.php
@@ -1,4 +1,10 @@
 <?php
+/* 运行模式 */
+# define('APP_MODE', 'pro');
+/* 你可以配置系统的默认模块配置 */
+# define('DEFAULT_MODULE', 'app');
+/* 如果需要开启URL别名请取消下一行注释，并配置modules/alias.php */
+# define('ALIAS_ENABLED', true);
 /* 如果你的网站以集群的方式提供服务时，请取消下一行的注释，并配置cluster_config.php */
 # define('RUN_IN_CLUSTER', true);
 /* 如果你的应用不是运行在网站的根目录,请取消下一行注释并修改其值,必须以/开始,以/结束。*/
@@ -17,10 +23,14 @@
 # define ('CONF_DIR', 'conf' );
 /* 如果你想改libs目录名，请取消下一行注释并修改其值. */
 # define ('LIBS_DIR', 'includes' );
-/* 是否开启gzip压缩 */
-# define('GZIP_ENABLED', true);
+/* 是否开启防雪崩机制 */
+# define('ANTI_AVALANCHE', true);
+/* 是否开启防CC机制（访问次数/每多少秒），需要在ccredis_config.php配置redis支持 */
+# define('ANTI_CC','10/60');
 /* 重新定义运行时内存限制 */
 # define ('RUNTIME_MEMORY_LIMIT', '128M' );
+/* 是否开启gzip压缩 */
+define('GZIP_ENABLED', true);
 /* 如果你要重新定义扩展加载器,请修改 */
 # define('EXTENSION_LOADER_CLASS', 'wulaphp\app\ExtensionLoader');
 /* 如果你要重新定义配置加载器,请修改 */
@@ -32,6 +42,7 @@
 // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!以下内容不可修改!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 // //////////////////////////////////////////////////////////////////////////////
 define('APPROOT', __DIR__ . DIRECTORY_SEPARATOR);
+# define('WULACMF_WEB_INSTALLER', 1);
 defined('PUBLIC_DIR') or define('PUBLIC_DIR', 'wwwroot');
 if (!defined('WWWROOT')) {
 	define('WWWROOT', APPROOT . PUBLIC_DIR . DIRECTORY_SEPARATOR);
